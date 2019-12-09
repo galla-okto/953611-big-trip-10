75a44dcb4b8e036bdaffe04661f17a01989c00a9
galla-okto <galla.okto1@gmail.com>
2019-12-09 22:31:02 +0200

    Добавляет ;

--- src/main.js
+++ src/main.js
@@ -8,11 +8,6 @@ import {calculatePriceTrip} from './components/price-trip.js';
 import {createPriceTripTemplate} from './components/price-trip.js';
 import {createTripSortTemplate} from './components/trip-sort.js';
 
-import {createTripDaysOpenTemplate} from './components/trip-day.js';
-import {createTripDaysCloseTemplate} from './components/trip-day.js';
-import {createTripDayOpenTemplate} from './components/trip-day.js';
-import {createTripDayCloseTemplate} from './components/trip-day.js';
-
 import {generateCards} from './mock/card.js';
 import {generateSiteMenu} from './mock/site-menu.js';
 import {generateFilters} from './mock/filter.js';
@@ -21,27 +16,8 @@ import {render, RenderPosition} from './utils.js';
 
 const CARD_COUNT = 5;
 
-const render2 = (container, template, place) => {
+const renderHtml = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
-};
-
-const renderCard = (card) => {
-  const cardComponent = new CardComponent(card);
-  const cardEditComponent = new CardEditComponent(card);
-
-  const editButton = cardComponent.getElement().querySelector(`.event__rollup-btn`);
-
-  editButton.addEventListener(`click`, () => {
-    CardListElement.replaceChild(cardEditComponent.getElement(), cardComponent.getElement());
-  });
-
-  const editForm = CardEditComponent.getElement().querySelector(`form`);
-
-  editForm.addEventListener(`submit`, () => {
-    CardListElement.replaceChild(cardComponent.getElement(), cardEditComponent.getElement());
-  });
-
-  render(CardListElement, cardComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
 const siteHeaderElement = document.querySelector(`.trip-main`);
@@ -54,7 +30,7 @@ const infoTrip = generateInfoTrip(cards);
 render(siteTripInfoElement, new InfoTripComponent(infoTrip).getElement(), RenderPosition.AFTERBEGIN);
 
 const priceTrip = calculatePriceTrip(cards);
-render2(siteTripInfoElement, createPriceTripTemplate(priceTrip), RenderPosition.BEFOREEND);
+renderHtml(siteTripInfoElement, createPriceTripTemplate(priceTrip), RenderPosition.BEFOREEND);
 
 const filters = generateFilters();
 render(siteTripControlsElement, new FilterComponent(filters).getElement(), RenderPosition.AFTERBEGIN);
@@ -65,40 +41,27 @@ render(siteTripControlsElement, new SiteMenuComponent(siteMenu).getElement(), Re
 const sitePageMainElement = document.querySelector(`.page-main`);
 const siteTripEventsElement = sitePageMainElement.querySelector(`.trip-events`);
 
+renderHtml(siteTripEventsElement, createTripSortTemplate(), RenderPosition.BEFOREEND);
+
 const CardListElement = siteTripEventsElement.querySelector(`.trip-days`);
 
-render2(siteTripEventsElement, createTripSortTemplate(), RenderPosition.BEFOREEND);
+const renderCard = (card) => {
+  const cardComponent = new CardComponent(card);
+  const cardEditComponent = new CardEditComponent(card);
 
-cards.forEach((card) => renderCard(card));
+  const editButton = cardComponent.getElement().querySelector(`.event__rollup-btn`);
 
-//render(CardListElement, new CardComponent(card).getElement(), RenderPosition.BEFOREEND));
+  editButton.addEventListener(`click`, () => {
+    CardListElement.replaceChild(cardEditComponent.getElement(), cardComponent.getElement());
+  });
 
-/*const days = [];
+  const editForm = cardEditComponent.getElement().querySelector(`form`);
 
-for (let index = 1; index < cards.length; index++) {
-  days.push(cards[index].date);
+  editForm.addEventListener(`submit`, () => {
+    CardListElement.replaceChild(cardComponent.getElement(), cardEditComponent.getElement());
+  });
+
+  render(CardListElement, cardComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-days.sort();*/
-
-//render2(siteTripEventsElement, createTripDaysOpenTemplate(), `beforeend`);
-
-//days.forEach((day) => {
-  //render2(siteTripEventsElement, createTripDayOpenTemplate(day), `beforeend`);
-
-  //for (let index = 1; index < cards.length; index++) {
-    //if (day === cards[index].date) {
-
-      //renderCard(cards[index]);
-
-      //render(siteTripEventsElement, new Card(cards[index]).getElement(), RenderPosition.BEFOREEND);
-
-      //render(siteTripEventsElement, new CardEdit(cards[index]).getElement(), RenderPosition.BEFOREEND);
-    //}
-  //};
-
-  //render2(siteTripEventsElement, createTripDayCloseTemplate(), `beforeend`);
-//});
-
-//render2(siteTripEventsElement, createTripDaysCloseTemplate(), `beforeend`);
-
+cards.forEach((card) => renderCard(card));
