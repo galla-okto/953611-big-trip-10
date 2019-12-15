762616acf9766e9ffd63202264989f064b2a1bd8
galla-okto <galla.okto1@gmail.com>
2019-12-15 21:50:04 +0200

    Выносит отдельно функции заменить и удалить

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,19 +1,36 @@
 import CardComponent from '../components/card.js';
 import CardEditComponent from '../components/card-edit.js';
 
-import {render, RenderPosition} from '../utils/render.js';
+import {render, replace, RenderPosition} from '../utils/render.js';
 
 const renderCard = (cardListElement, card) => {
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  const replaceEditToCard = () => {
+    replace(cardComponent, cardEditComponent);
+  };
+
+  const replaceCardToEdit = () => {
+    replace(cardEditComponent, cardComponent);
+  };
+
   const cardComponent = new CardComponent(card);
-  const cardEditComponent = new CardEditComponent(card);
 
   cardComponent.setEditButtonClickHandler(() => {
-    cardListElement.replaceChild(cardEditComponent.getElement(), cardComponent.getElement());
+    replaceCardToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
   });
 
-  cardEditComponent.setSubmitHandler(() => {
-    cardListElement.replaceChild(cardComponent.getElement(), cardEditComponent.getElement());
-  });
+  const cardEditComponent = new CardEditComponent(card);
+
+  cardEditComponent.setSubmitHandler(replaceEditToCard);
 
   render(cardListElement, cardComponent, RenderPosition.BEFOREEND);
 };
